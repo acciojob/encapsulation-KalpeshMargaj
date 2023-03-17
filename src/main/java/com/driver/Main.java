f2928d5972c6e16f57236346de238de82ec636bc
KalpeshMargaj <117845849+KalpeshMargaj@users.noreply.github.com>
2023-03-17 10:42:43 +0530

    check this

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args)
+    {
+        RWOnly r = new RWOnly();
+//        System.out.println(r.name + "0");
+//        java: name has private access in com.driver.RWOnly
+        r.setter("Kalpesh");
+        String ans = r.getter();
+        System.out.println(ans);
+    }
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -3,5 +3,13 @@ package com.driver;
 public class RWOnly {
     private String name = "Suraj";
 
+    public void setter(String name)
+    {
+        this.name=name;
+    }
+    public String getter()
+    {
+        return name;
+    }
     
 }
